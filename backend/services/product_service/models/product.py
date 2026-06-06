fc30f9be5aac0e49312964b33e6a769cfcefb9af
MKhuii <lyminhkhoi15122005@gmail.com>
2026-06-06 22:49:20 +0700

    Updating folder

--- backend/services/product_service/models/product.py
+++ backend/services/product_service/models/product.py
@@ -4,4 +4,6 @@ class Product(SQLModel, table=True):
     id: int = Field(primary_key=True, index=True),
     name: str = Field(unique=True, index=True, nullable=False)
     description: str = Field(nullable=False)
-    
+    quantity: int = Field(default=0, nullable=False)
+    price: int = Field(default=0, nullable=False)
+    product_img: bytes
